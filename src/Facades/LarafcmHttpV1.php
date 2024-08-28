e7e2009a1269313ed89f5eea42e3cfe0fa098ab2
Marcos <marcosanibalgg@gmail.com>
2024-08-28 00:41:07 -0400

    Create Facade

---
+++ src/Facades/LarafcmHttpV1.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Muchg0di\LarafcmHttpV1\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class LarafcmHttpV1 extends Facade
+{
+    protected static function getFacadeAccessor()
+    {
+        return 'larafcm-http-v1';
+    }
+}
